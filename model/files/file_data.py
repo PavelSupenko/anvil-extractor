7d075c3776c440ba736bf846c4d90a622c2474bc
Pavlo Supenko <pavlo.supenko@bini.games>
2023-12-08 10:22:37 +0200

    Add more info to data file type

--- model/files/file_data.py
+++ model/files/file_data.py
@@ -1,10 +1,23 @@
 class FileData:
-    def __init__(self, name: str, file_id: int, depth: int):
+    def __init__(self, path: str, file_id: int, depth: int):
         self.file_id = file_id
-        self.name = name
+        self.path = path
+
+        self.compressed = False
+        self.data = None
+
+        file_name = path.split('/')[-1].split('.')
+        self.name = file_name[0]
+        self.type = file_name[1] if len(file_name) > 1 else None
 
         self.depth = depth
         self.children = []
+
+    def mark_as_compressed(self):
+        self.compressed = True
+
+    def mark_as_uncompressed(self):
+        self.compressed = False
 
     def __str__(self):
         return f"{self.name}\t\t{self.file_id:016X}"
