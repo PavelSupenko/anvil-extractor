cf4185b0be3b689790ad62363aba83f6cba4a0f9
Pavlo Supenko <pavlo.supenko@bini.games>
2023-12-24 17:58:37 +0200

    Add texture class from ACU to AC2 and fix file id datatype for AC2

--- games/ac2/ac2_game_data.py
+++ games/ac2/ac2_game_data.py
@@ -6,5 +6,6 @@ from model.game.game_data import GameData
 
 class AC2GameData(GameData):
     def __init__(self, path: str):
-        file_types = json.load(open(f"{os.path.dirname(__file__)}/resource_types.json"))
-        super().__init__(path, data_file_format=1, file_types=file_types)
+        # file_types = json.load(open(f"{os.path.dirname(__file__)}/resource_types.json"))
+        file_types = {}
+        super().__init__(path, data_file_format=1, file_types=file_types, file_id_datatype="I")
