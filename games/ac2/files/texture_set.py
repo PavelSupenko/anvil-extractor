20a44dd61bb98cde7678633ecb9786006477c7d8
Pavlo Supenko <pavlo.supenko@bini.games>
2024-01-06 11:49:03 +0200

    Fix texture set reader because AC2 version have 11 textures data instead of 12 in ACU
    120 bytes count is from texture set start to the last texture id. Id's is 4-bytes (8-bytes in ACU) and delimiter between them is 0101 or 0300 (As I understand first is for valid textures and second variant is for not set textures for that model)

--- games/ac2/files/texture_set.py
+++ games/ac2/files/texture_set.py
@@ -12,12 +12,12 @@ class Reader(BaseFile, Material):
         Material.__init__(self, None)
 
     def read(self, file_id: int, file: FileDataWrapper):
-        texture_table = file.read_numpy([('', '<u2'), ('texture_id', '<u8')], 120)
+        texture_table = file.read_numpy([('', '<u2'), ('texture_id', '<u4')], 66)
         self.diffuse, self.normal, self.specular, \
             self.height, tex5, self.transmission, tex7, \
-            self.mask1, self.mask2, tex10, tex11, tex12 \
+            self.mask1, self.mask2, tex10, tex11 \
             = [texture_id if texture_id != 0 else None for texture_id in texture_table['texture_id']]
 
-        for var, pos in [[tex5, 5], [tex7, 7], [tex10, 10], [tex11, 11], [tex12, 12]]:
+        for var, pos in [[tex5, 5], [tex7, 7], [tex10, 10], [tex11, 11]]:
             if var is not None:
                 logging.info(f'found a texture set with an id in position {pos}')
