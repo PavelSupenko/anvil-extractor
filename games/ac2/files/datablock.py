b3c435672a05aaf582ac9fa79d70e0e11d1373df
Pavlo Supenko <pavlo.supenko@bini.games>
2024-01-07 15:50:31 +0200

    Edit data block reading to work with AC2

--- games/ac2/files/datablock.py
+++ games/ac2/files/datablock.py
@@ -8,11 +8,8 @@ class Reader(BaseFile):
         count1 = file.read_uint_32()
         self.files = []
         for _ in range(count1):
-            file.indent()
-            file.read_bytes(2)
+            file.read_bytes(2)  # 0100 as a delimiter
             self.files.append(file.read_file_id())
-            file.indent(-1)
         file.out_file_write('\n')
         count2 = file.read_uint_32()  # seems to be about the same or slightly less than count1
         self.cell_id = file.read_uint_32()
-        file.read_bytes(4)  # this might be part of the previous as a 64 bit uint
