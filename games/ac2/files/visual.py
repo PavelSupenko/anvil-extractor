8d62018f0eb23233c00b4b889748f4ddb6a94ace
Pavlo Supenko <pavlo.supenko@bini.games>
2024-01-07 15:50:45 +0200

    Add visual reader from ACU

---
+++ games/ac2/files/visual.py
@@ -0,0 +1,24 @@
+from model.files.base_file import BaseFile
+from model.files.file_data_wrapper import FileDataWrapper
+
+
+class Reader(BaseFile):
+    ResourceType = 0xEC658D29
+
+    def read(self, file_id: int, file: FileDataWrapper):
+        file.read_bytes(4)
+        file.read_file_id()
+
+        file.read_bytes(1)
+
+        self.nested_files = {}
+
+        nested_file = file.read_file()
+        self.nested_files[nested_file.resource_type] = nested_file
+
+        file.read_bytes(1)
+
+        file.out_file_write('\n')
+
+        for _ in range(7):
+            file.read_float_32()
